9f745823caa24fdda2c089fdbe7eaa7916f730ba
bopin <bopin@code.com>
2024-05-26 13:43:17 +0800

    add SECURE version

--- IntegerOverflow.hpp
+++ IntegerOverflow.hpp
@@ -31,19 +31,31 @@ namespace Vuln {
 	size be checked  with caller function!
 	*/
 	void IntegerDowngrade::VulnFunc(uint32 size) {
+#ifdef SECURE
+		if (size > 8) {
+			size = size % 9;
+		}
+#else
+		// nothing
+#endif
+
 		char data[8] = { 0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07 };
 		char* buf = (char*)LocalAlloc(0x40,8);
-		try
+		__try
 		{
 			// stack overflow here
 			memset(data, 0, size);
+			dump_hex_top2("hex: ",data, ARRAYSIZE(data));
+			hexdump2(data, ARRAYSIZE(data));
 			// integer downgrade here
 			memcpy(buf, data, size);
+			dump_hex_top2("hex: ", buf, 8);
+			hexdump2(buf, 8);
 		}
-		catch (const char* msg)
+		__except (EXCEPTION_ACCESS_VIOLATION)
 		{
 			EPrint("IntegerDowngrade trigger");
-			EPrint(msg);
+			EPrint("error code: %d", GetExceptionCode());
 		}
 		LocalFree(buf);
 	}
