707636f52a04c2c203273c50f6b60040ff21d671
bopin <bopin@code.com>
2024-09-09 11:20:31 +0800

    update wchar_t leak

--- NonZeroEnd.hpp
+++ NonZeroEnd.hpp
@@ -10,6 +10,34 @@
 
 #define BUF_SIZE 0x10
 
+int memsetw(wchar_t* buf, unsigned int val, unsigned int size) {
+	if (val > 0xff) {
+		return 0x01;
+	}
+	if (buf == NULL || (DWORD_PTR)buf < 0x1000) {
+		return 0x02;
+	}
+
+	for (size_t i = 0; i < size; i++)
+	{
+		buf[i] = val;
+	}
+	buf[size] = 0x00;
+
+	unsigned int buf_size = 0;
+	do
+	{
+		buf_size++;
+	} while (*(WORD*)((DWORD_PTR)buf + 2 * buf_size) != 0);
+
+	if (buf_size == size) {
+		return 0x00;
+	}
+	memset(buf, 0, size);
+	return 0x04;
+}
+
+
 namespace Vuln {
 	/**
 	* 
@@ -23,8 +51,8 @@ namespace Vuln {
 	public:
 		ERROR_T Execute(V_PARAS* args);
 	private:
-		ERROR_T SetData(char* data, int size);
-		ERROR_T GetData(char** data, int* outsize);
+		ERROR_T SetData(wchar_t* data, int size);
+		ERROR_T GetData(wchar_t** data, int* outsize);
 		PVOID Buf;
 	private:
 	};
@@ -44,18 +72,23 @@ namespace Vuln {
 
 	ERROR_T NonZeroEnd::Execute(V_PARAS* args) {
 
-		char* poc_data = (char*)VirtualAlloc(0,BUF_SIZE,MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-		memset(poc_data, 0x41, BUF_SIZE);
-		int size = strlen(poc_data);
+		wchar_t* poc_data = (wchar_t*)malloc(BUF_SIZE);
+		if (memsetw(poc_data, 0x41, BUF_SIZE)) {
+			DPrint("memsetw failed \n");
+			return 0xffffeeee;
+		}
 
-		*(BYTE*)(DWORD_PTR)(poc_data + size) = 0x41;
+		int size = lstrlenW(poc_data);
+
+
+		*(WORD*)((DWORD_PTR)poc_data + BUF_SIZE * 2) = 0x43;
 
 		SetData(poc_data, size);
-		VirtualFree(poc_data, BUF_SIZE,MEM_DECOMMIT | MEM_RELEASE);
+		free(poc_data);
 #ifdef SECURE
 
 #else
-		char* buf = 0;
+		wchar_t* buf = 0;
 		int outsize = 0;
 		GetData(&buf, &outsize);
 		DPrint("information disclosure size: 0x%x %d", outsize,outsize);
@@ -64,16 +97,16 @@ namespace Vuln {
 		return 0x40000001;
 	}
 
-	ERROR_T NonZeroEnd::SetData(char* data, int size) {
+	ERROR_T NonZeroEnd::SetData(wchar_t* data, int size) {
 		Buf = malloc(size);
 		DPrint("Buf mem addr: 0x%x",Buf)
 		memcpy(Buf, data, size);
 		return 0x00;
 	}
 
-	ERROR_T NonZeroEnd::GetData(char** data, int* outsize) {
-		*outsize = strlen((char*)Buf);
-		*data = (char*)malloc(*outsize);
+	ERROR_T NonZeroEnd::GetData(wchar_t** data, int* outsize) {
+		*outsize = lstrlenW((wchar_t*)Buf);
+		*data = (wchar_t*)malloc(*outsize);
 		memcpy(*data, Buf, *outsize);
 		return 0x00;
 	}
